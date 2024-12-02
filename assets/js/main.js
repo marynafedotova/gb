8c1a947562c79384e1a922312581624a34201f2d
marynafedotova <145754992+marynafedotova@users.noreply.github.com>
2024-12-02 12:38:56 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -345,3 +345,99 @@ document.querySelectorAll('.catalog-list a').forEach(link => {
     window.location.href = pageUrl;
   });
 });
+// form
+document.addEventListener('DOMContentLoaded', function () {
+  const form = document.getElementById('feedback_form');
+  const nameFld = document.getElementById('exampleInputName');
+  const telFld = document.getElementById('exampleInputTel');
+
+  if (!form || !nameFld || !telFld) {
+    console.error('Form or fields not found!');
+    return;
+  }
+
+  form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const name = nameFld.value ? nameFld.value.trim() : ''; 
+    const tel = telFld.value ? telFld.value.trim() : '';
+
+    const errors = [];
+
+    // Очистка классов ошибок
+    nameFld.classList.remove('is-invalid');
+    telFld.classList.remove('is-invalid');
+
+    if (name === '') {
+      errors.push("Введіть, будь ласка, Ваше ім'я");
+      nameFld.classList.add('is-invalid');
+    } else if (name.length < 2) {
+      errors.push('Ваше ім\'я занадто коротке');
+      nameFld.classList.add('is-invalid');
+    }
+
+    if (tel === '' || tel.length < 17) { 
+      errors.push('Введіть, будь ласка, правильний номер телефону');
+      telFld.classList.add('is-invalid');
+    }
+
+    if (errors.length > 0) {
+      toast.error(errors.join('. '));
+      return;
+    }
+
+    const CHAT_ID = '-1002278785620';
+    const BOT_TOKEN = '8046931960:AAHhJdRaBEv_3zyB9evNFxZQlEdiz8FyWL8';
+    const message = `<b>Ім'я: </b> ${name}\r\n<b>Телефон: </b>${tel}`;
+    const url = `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage?chat_id=${CHAT_ID}&text=${encodeURIComponent(message)}&parse_mode=HTML`;
+
+    fetch(url, {
+      method: 'POST',
+    })
+    .then(response => response.json())
+    .then(data => {
+      if (data.ok) {
+        nameFld.value = '';
+        telFld.value = '';
+        toast.success('Ваше повідомлення успішно надіслано.');
+      } else {
+        toast.error('Сталася помилка.');
+      }
+    })
+    .catch(error => {
+      toast.error('Помилка: ' + error.message);
+    });
+  });
+
+  telFld.addEventListener('input', function (e) {
+    let input = e.target.value.replace(/\D/g, '');
+    let formattedInput = '';
+
+    if (input.length > 0) {
+      formattedInput += '+38 (';
+    }
+    if (input.length >= 1) {
+      formattedInput += input.substring(0, 3);
+    }
+    if (input.length >= 4) {
+      formattedInput += ') ' + input.substring(3, 6);
+    }
+    if (input.length >= 7) {
+      formattedInput += '-' + input.substring(6, 8);
+    }
+    if (input.length >= 9) {
+      formattedInput += '-' + input.substring(8, 10);
+    }
+
+    e.target.value = formattedInput;
+  });
+  nameFld.addEventListener('input', function (e) {
+  let input = e.target.value;
+   e.target.value = input.replace(/[^A-Za-zА-Яа-яІіЇїЄє']/g, '');
+});
+});
+//copiraite
+document.addEventListener("DOMContentLoaded", function() {
+  const currentYear = new Date().getFullYear();
+  document.getElementById("year").textContent = currentYear;
+});
